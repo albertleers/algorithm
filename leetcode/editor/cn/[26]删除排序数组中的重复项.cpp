f55546f57603f2de7dddcc82c7a8e8248f589395
Albert_Lee <wx)liwufeng@163.com>
2020-09-14 13:54:49 +0800

    26

--- leetcode/editor/cn/[26]删除排序数组中的重复项.cpp
+++ leetcode/editor/cn/[26]删除排序数组中的重复项.cpp
@@ -48,7 +48,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        if (nums.empty()) return 0;
+        int index = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[index - 1]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
